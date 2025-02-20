cbb14585510af69f633c38215afdd986375afcb5
levan <levan@galleon.so>
2025-02-20 11:39:06 +0200

    updated test implementation of IAP store

--- Runtime/TestScript.cs
+++ Runtime/TestScript.cs
@@ -1,9 +1,44 @@
+using System.Collections.ObjectModel;
 using UnityEngine;
+using UnityEngine.Purchasing;
+using UnityEngine.Purchasing.Extension;
 
-public class TestScript : MonoBehaviour
+public class TestScript : IStore
 {
-    void Start()
+    //////////////////////////////////////////////////////////////////////////////////////// Members
+
+    private IStoreCallback callback;
+    
+    //////////////////////////////////////////////////////////////////////////////////////// IStore Methods
+    
+    public void Initialize(IStoreCallback callback)
     {
-        Debug.Log("Hello World!");
+        this.callback = callback;
+        Debug.Log("===============================");
+        Debug.Log($"IAP - Store Initialized");
+        Debug.Log("===============================");
+    }
+
+    public void RetrieveProducts(ReadOnlyCollection<ProductDefinition> products)
+    {
+        Debug.Log("===============================");
+        Debug.Log("IAP - Retrieving Products :");
+        foreach (var product in products)
+            Debug.Log($"- {product.id} ({product.type})");
+        Debug.Log("===============================");
+    }
+
+    public void Purchase(ProductDefinition product, string developerPayload)
+    {
+        Debug.Log("===============================");
+        Debug.Log($"IAP - Purchasing {product.id} ({product.type})");
+        Debug.Log("===============================");
+    }
+
+    public void FinishTransaction(ProductDefinition product, string transactionId)
+    {
+        Debug.Log("===============================");
+        Debug.Log($"IAP - Finished transaction for : {product.id} ({product.type})");
+        Debug.Log("===============================");
     }
 }
