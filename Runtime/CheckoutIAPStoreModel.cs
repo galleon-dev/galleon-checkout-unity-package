b8aa2cd28d91cf6fc00779677d040949fcc07936
levan <levan@galleon.so>
2025-02-20 12:05:06 +0200

    made CheckoutIAPStoreModel a singleton

--- Runtime/CheckoutIAPStoreModel.cs
+++ Runtime/CheckoutIAPStoreModel.cs
@@ -4,6 +4,22 @@ using UnityEngine.Purchasing.Extension;
 
 public class CheckoutIAPStoreModel : AbstractPurchasingModule
 {
+    //////////////////////////////////////////////////////////// Singleton
+    
+    private static CheckoutIAPStoreModel instance;
+    public  static CheckoutIAPStoreModel Instance
+    {
+        get
+        {
+            if (instance == null)
+                instance = new CheckoutIAPStoreModel();
+            
+            return instance;
+        }
+    }
+    
+    //////////////////////////////////////////////////////////// AbstractPurchasingModule Methods 
+    
     public override void Configure()
     {
         RegisterStore("GalleonCheckout", InstantiateStore());
