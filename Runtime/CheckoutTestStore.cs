dd8169dbc5c36319c3bc267c758610fed01dde99
levan <levan@galleon.so>
2025-02-20 12:36:57 +0200

    updated test store

--- Runtime/CheckoutTestStore.cs
+++ Runtime/CheckoutTestStore.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using UnityEngine;
 using UnityEngine.Purchasing;
@@ -16,6 +17,7 @@ namespace Galleon.Checkout
         public void Initialize(IStoreCallback callback)
         {
             this.callback = callback;
+            
             Debug.Log("===============================");
             Debug.Log($"IAP - Store Initialized");
             Debug.Log("===============================");
@@ -23,11 +25,27 @@ namespace Galleon.Checkout
 
         public void RetrieveProducts(ReadOnlyCollection<ProductDefinition> products)
         {
+            ////////////////////////////////////////////////////////////////////////////
             Debug.Log("===============================");
             Debug.Log("IAP - Retrieving Products :");
             foreach (var product in products)
                 Debug.Log($"- {product.id} ({product.type})");
             Debug.Log("===============================");
+            ////////////////////////////////////////////////////////////////////////////
+            
+            var productDescriptions = new List<ProductDescription>();
+            
+            foreach (var product in products)
+            {   
+                productDescriptions.Add(new ProductDescription(id       : product.storeSpecificId
+                                                              ,metadata : new ProductMetadata(priceString:    "$9.99"
+                                                                                             ,title:          "My Product"
+                                                                                             ,description:    "My Product Description"
+                                                                                             ,currencyCode:   "USD"
+                                                                                             ,localizedPrice: 9.99m)));
+            }
+            
+            this.callback.OnProductsRetrieved(productDescriptions);
         }
 
         public void Purchase(ProductDefinition product, string developerPayload)
@@ -35,6 +53,12 @@ namespace Galleon.Checkout
             Debug.Log("===============================");
             Debug.Log($"IAP - Purchasing {product.id} ({product.type})");
             Debug.Log("===============================");
+            
+            // Simulate a successful purchase
+            this.callback.OnPurchaseSucceeded(storeSpecificId       : product.storeSpecificId
+                                             ,receipt               : "receipt"
+                                             ,transactionIdentifier : "transactionId"
+            );
         }
 
         public void FinishTransaction(ProductDefinition product, string transactionId)
