805a527d209956ba327794610ce923abebc1e268
EdgarasArt <himixlt@gmail.com>
2025-06-25 04:06:36 +0300

    Additional UI Adjustments
    ++ Updated: Credit Card removal confirmation screen (UI)
    ++ Updated: "Settings page — you can remove the legal notice."
    ++ Updated: "You can remove the legal notice on the Payment Succeeded page."
    ++ Some Text Color Update

--- Runtime/FooterCheckup/ManageFooterText.cs
+++ Runtime/FooterCheckup/ManageFooterText.cs
@@ -6,8 +6,8 @@ namespace Galleon.FooterText
     {
         public static ManageFooterText Instance { get; private set; }
 
-        public GameObject ViewPayemtnMethodsPanel;
-
+        public GameObject ViewPaymentMethodsPanel;
+        public GameObject TermsOfServicePanel;
         private void Awake()
         {
             Instance = this;
@@ -15,8 +15,14 @@ namespace Galleon.FooterText
 
         public void Show(bool _Status)
         {
-            Debug.Log("Show Footer: " + _Status);
-            ViewPayemtnMethodsPanel.SetActive(_Status);
+            Debug.Log("Show View Payment Methods Panel: " + _Status);
+            ViewPaymentMethodsPanel.SetActive(_Status);
+        }
+
+        public void ShowTermsOfService(bool _Status)
+        {
+            Debug.Log("Show Terms Of Service Panel: " + _Status);
+            TermsOfServicePanel.SetActive(_Status);
         }
 
     }
