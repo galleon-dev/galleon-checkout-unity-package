f07b0ea7f9831b66a317fcf2de26622734c465db
levan <levan@galleon.so>
2026-02-16 11:34:21 +0200

    Removes Checkout IAP Store implementation.
    Removes the custom Checkout IAP store implementation.
    The files for the custom IAP store and listener are deleted.
    This change removes the usage of a custom IAP solution.

--- Runtime/CheckoutCustomIAPStore/CheckoutIAPStoreModel.cs
+++ Runtime/CheckoutCustomIAPStore/CheckoutIAPStoreModel.cs
@@ -1,32 +1,32 @@
-using Galleon.Checkout;
-using UnityEngine;
-using UnityEngine.Purchasing.Extension;
+//using Galleon.Checkout;
+//using UnityEngine;
+//using UnityEngine.Purchasing.Extension;
 
-public class CheckoutIAPStoreModel : AbstractPurchasingModule
-{
-    //////////////////////////////////////////////////////////// Singleton
-    
-    private static CheckoutIAPStoreModel instance;
-    public  static CheckoutIAPStoreModel Instance
-    {
-        get
-        {
-            if (instance == null)
-                instance = new CheckoutIAPStoreModel();
-            
-            return instance;
-        }
-    }
-    
-    //////////////////////////////////////////////////////////// AbstractPurchasingModule Methods 
-    
-    public override void Configure()
-    {
-        //RegisterStore("GalleonCheckout", InstantiateStore());
-    }
-
-    // private IStore InstantiateStore()
-    // {
-    //     return new CheckoutIAPStore();
-    // }
-}
+// public class CheckoutIAPStoreModel : AbstractPurchasingModule
+// {
+//     //////////////////////////////////////////////////////////// Singleton
+//     
+//     private static CheckoutIAPStoreModel instance;
+//     public  static CheckoutIAPStoreModel Instance
+//     {
+//         get
+//         {
+//             if (instance == null)
+//                 instance = new CheckoutIAPStoreModel();
+//             
+//             return instance;
+//         }
+//     }
+//     
+//     //////////////////////////////////////////////////////////// AbstractPurchasingModule Methods 
+//     
+//     public override void Configure()
+//     {
+//         //RegisterStore("GalleonCheckout", InstantiateStore());
+//     }
+// 
+//     // private IStore InstantiateStore()
+//     // {
+//     //     return new CheckoutIAPStore();
+//     // }
+// }
