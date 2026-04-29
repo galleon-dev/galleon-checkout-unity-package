75280e3f20af561c4e8c6a2db3eb23826079cb5a
levan <levan@galleon.so>
2026-04-29 14:15:35 +0300

    fixed icons bug

--- Runtime/ResourceManager/CheckoutSprites.cs
+++ Runtime/ResourceManager/CheckoutSprites.cs
@@ -66,7 +66,7 @@ namespace Galleon.Checkout
             }
             else if (SpriteResources.Any(x => x.Name == pmTypeOrURL + "_icon"))
             {
-                return SpriteResources.FirstOrDefault(x => x.Name == pmTypeOrURL)?.Sprite;
+                return SpriteResources.FirstOrDefault(x => x.Name == pmTypeOrURL + "_icon")?.Sprite;
             }
             else
             {
